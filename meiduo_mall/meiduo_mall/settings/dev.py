54d81c749bcae4a31cba78eeb0f59500761e65a3
zy723 <zy723@vip.qq.com>
2020-03-25 14:24:05 +0800

    office PC init

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -97,8 +97,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
         'HOST': '127.0.0.1',  # 数据库主机
         'PORT': 3306,  # 数据库端口
-        'USER': 'xiaofan',  # 数据库用户名
-        'PASSWORD': '123456',  # 数据库用户密码
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': 'root',  # 数据库用户密码
         'NAME': 'meiduo'  # 数据库名字
     },
 }
